c6d25a67ceb77c9b2e1b6f8c60deffadb37bafba
Emanuel C. Barros <emanuelcavalcanteb@gmail.com>
2023-06-26 23:30:16 -0300

    Remove unused code.

--- app/Domain/Features/GetBook.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Domain\Features;
-
-interface GetBook
-{
-
-}
--- app/Domain/Features/SaveBook.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Domain\Features;
-
-interface SaveBook
-{
-    public function handle(array $data);
-}
